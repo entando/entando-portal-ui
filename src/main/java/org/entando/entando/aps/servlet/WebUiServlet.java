82b415182b28a646a589ba4f50f080427ef713c6
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2022-01-03 11:28:28 +0100

    ENG-2021: Improvement of web-ui servlet

--- src/main/java/org/entando/entando/aps/servlet/WebUiServlet.java
+++ src/main/java/org/entando/entando/aps/servlet/WebUiServlet.java
@@ -31,6 +31,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.entando.entando.ent.exception.EntException;
 import org.entando.entando.ent.util.EntLogging.EntLogFactory;
@@ -43,11 +44,15 @@ public class WebUiServlet extends AbstractFrontEndServlet {
     
     private static final EntLogger logger = EntLogFactory.getSanitizedLogger(StartupListener.class);
     
-    private final String WEB_UI_ENABLED = "WEB_UI_ENABLED";
+    public static final String PAGE_CODE_KEY = "pageCode";
+    public static final String LANG_CODE_KEY = "langCode";
+    public static final String USERNAME_KEY = "username";
+    public static final String BASE_URL_KEY = "applicationBaseURL";
+    public static final String CSP_TOKEN_KEY = "cspToken";
     
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String webUiEnabled = System.getenv(SystemConstants.CSP_HEADER_ENABLED);
+        String webUiEnabled = System.getenv(SystemConstants.WEB_UI_ENABLED);
         if (StringUtils.isEmpty(webUiEnabled) || !Boolean.TRUE.toString().equalsIgnoreCase(webUiEnabled)) {
             logger.warn("Web-UI servlet is not enabled");
             return;
@@ -61,7 +66,7 @@ public class WebUiServlet extends AbstractFrontEndServlet {
         } catch (IOException io) {
             logger.error("IO error", io);
             throw io;
-        } catch (Throwable t) {
+        } catch (Exception t) {
             logger.error("Error building response", t);
             throw new ServletException("Error building response", t);
         }
@@ -75,22 +80,26 @@ public class WebUiServlet extends AbstractFrontEndServlet {
         String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         try {
             JSONObject obj = new JSONObject(body);
-            String pageCode = obj.getString("pageCode");
-            String langCode = obj.getString("langCode");
-            String username = obj.getString("username");
+            String pageCode = this.getParamValue(obj, PAGE_CODE_KEY);
+            String langCode = this.getParamValue(obj, LANG_CODE_KEY);
+            String username = this.getParamValue(obj, USERNAME_KEY);
             ILangManager langManager = (ILangManager) ApsWebApplicationUtils.getBean(SystemConstants.LANGUAGE_MANAGER, request);
             Lang currentLang = (!StringUtils.isBlank(langCode)) ? langManager.getLang(langCode) : null;
             if (null == currentLang) {
+                if (!StringUtils.isBlank(langCode)) {
+                    logger.warn("Invalid language requested: '{}'", langCode);
+                }
                 currentLang = langManager.getDefaultLang();
             }
             UserDetails currentUser = null;
             IUserManager userManager = (IUserManager) ApsWebApplicationUtils.getBean(SystemConstants.USER_MANAGER, request);
-            if (StringUtils.isBlank(username) || username.equalsIgnoreCase(SystemConstants.GUEST_USER_NAME)) {
+            if (StringUtils.isBlank(username) || SystemConstants.GUEST_USER_NAME.equalsIgnoreCase(username)) {
                 currentUser = userManager.getGuestUser();
             } else {
                 IAuthenticationProviderManager authenticationProviderManager = (IAuthenticationProviderManager) ApsWebApplicationUtils.getBean(SystemConstants.AUTHENTICATION_PROVIDER_MANAGER, request);
                 currentUser = authenticationProviderManager.getUser(username);
                 if (null == currentUser) {
+                    logger.warn("Invalid user requested: '{}'", username);
                     currentUser = userManager.getGuestUser();
                 }
             }
@@ -98,18 +107,24 @@ public class WebUiServlet extends AbstractFrontEndServlet {
             IPageManager pageManager = (IPageManager) ApsWebApplicationUtils.getBean(SystemConstants.PAGE_MANAGER, request);
             IAuthorizationManager authManager = (IAuthorizationManager) ApsWebApplicationUtils.getBean(SystemConstants.AUTHORIZATION_SERVICE, request);
             IPage currentPage = (!StringUtils.isBlank(pageCode)) ? pageManager.getOnlinePage(pageCode) : null;
-            if (null == currentPage) {
-                String notFoundPageCode = configManager.getParam(SystemConstants.CONFIG_PARAM_NOT_FOUND_PAGE_CODE);
+            if (StringUtils.isBlank(pageCode)) {
+                String homepagePageCode = configManager.getParam(IPageManager.CONFIG_PARAM_HOMEPAGE_PAGE_CODE);
+                currentPage = pageManager.getOnlinePage(homepagePageCode);
+            } else if (null == currentPage) {
+                if (!StringUtils.isBlank(pageCode)) {
+                    logger.warn("Invalid page requested: '{}'", pageCode);
+                }
+                String notFoundPageCode = configManager.getParam(IPageManager.CONFIG_PARAM_NOT_FOUND_PAGE_CODE);
                 currentPage = pageManager.getOnlinePage(notFoundPageCode);
             } else if (!authManager.isAuth(currentUser, currentPage)) {
-                String loginPageCode = configManager.getParam(SystemConstants.CONFIG_PARAM_LOGIN_PAGE_CODE);
+                String loginPageCode = configManager.getParam(IPageManager.CONFIG_PARAM_LOGIN_PAGE_CODE);
                 currentPage = pageManager.getOnlinePage(loginPageCode);
             }
-            String applicationBaseURL = obj.getString("applicationBaseURL");
+            String applicationBaseURL = this.getParamValue(obj, BASE_URL_KEY);
             if (!StringUtils.isBlank(applicationBaseURL)) {
-                reqCtx.addExtraParam(SystemConstants.EXTRAPAR_WEBUI_APPL_BASE_URL, applicationBaseURL);
+                reqCtx.addExtraParam(SystemConstants.EXTRAPAR_WEB_UI_APPL_BASE_URL, applicationBaseURL);
             }
-            String cspToken = obj.getString("cspToken");
+            String cspToken = this.getParamValue(obj, CSP_TOKEN_KEY);
             if (!StringUtils.isBlank(cspToken)) {
                 reqCtx.addExtraParam(SystemConstants.EXTRAPAR_CSP_NONCE_TOKEN, cspToken);
             }
@@ -121,6 +136,13 @@ public class WebUiServlet extends AbstractFrontEndServlet {
             throw new EntException("Error reading body request", e);
         }
         return reqCtx;
+    }
+    
+    private String getParamValue(JSONObject obj, String paramName) throws JSONException {
+        if (obj.has(paramName)) {
+            return obj.getString(paramName);
+        }
+        return null;
     }
     
 }
